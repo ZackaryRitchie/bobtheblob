d790c7d0654b53484a5a28ceadb575bf0bb36c53
2551478 <2551478@cegepoutaouais.qc.ca>
2026-09-04 13:37:50 -0400

    ajoute classe emploi et completer

---
+++ LaVieDeBob/Emploi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LaVieDeBob
+{
+    public class Emploi
+    {
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public string Carriere { get; set; }
+        public int Salaire { get; set; }
+
+
+        public Emploi() 
+        {
+            Prenom = string.Empty;
+            Nom = string.Empty; 
+            Carriere = string.Empty;
+            Salaire=0;    
+        }
+
+        public Emploi(string prenom, string nom, string carriere, int salaire)
+        {
+            Prenom = prenom;
+            Nom = nom;
+            Carriere = carriere;
+            Salaire = salaire;
+        }
+
+
+        public int Paie(int NbHeureTravailler=1)
+        {
+            return Salaire* NbHeureTravailler;
+        }
+
+
+        public void AugSalaire(int augementation)
+        {
+            Salaire += augementation;
+        }
+    }
+}
