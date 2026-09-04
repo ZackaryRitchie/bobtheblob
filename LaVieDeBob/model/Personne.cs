8ea0df682094a3f5b6e34898716190a82c11363e
2551478 <2551478@cegepoutaouais.qc.ca>
2026-09-04 13:10:52 -0400

    création classe personne et completer

--- LaVieDeBob/model/Personne.cs
+++ LaVieDeBob/model/Personne.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices.ObjectiveC;
 
 
 namespace LaVieDeBob
@@ -6,26 +7,56 @@ namespace LaVieDeBob
 
 	public class Personne
 	{
+		public string Prenom {  get; set; }
 		public string Nom {  get; set; }
 		public object[] Animaux { get; set; }
-		public DateTime Date { get; set; }
-		
+		public string Emploi { get; set; }
+		public object Adresse { get; set; }
+		public int Age { get; set; }
 
 		public Personne()
 		{
+			Prenom = "";
 			Nom = "";
 			Animaux = [];
-			Date =  DateTime.Now ;
+			Emploi = "aucun";
+			Adresse = "";
+			Age = 0;
+
 		}
 
-		public Personne(string nom, object[] animaux)
+		public Personne(string prenom,string nom, object[] animaux, string adresse, int age)
 		{
+			Prenom= prenom;
 			Nom = nom;
 			Animaux=animaux;
-			Date=  DateTime.Now;
+			Emploi = "aucune";
+			Adresse = adresse;
+			Age=age;
 		}
-	}
 
+		public Personne(string prenom, string nom, object[] animaux,string emploi, object adresse,int age) 
+		{
+            Prenom = prenom;
+            Nom = nom; 
+			Animaux=animaux;
+			Emploi = emploi;
+			Adresse=adresse;
+			Age=age;
+
+		}
+
+
+		public void tempsavence(int annee)
+		{
+			Age = Age + annee;
+		}
+
+        public override string ToString()
+        {
+            return $"nom:{Prenom} {Nom}, Age: {Age} ans, Adresse: {Adresse}, Emploi: {Emploi}, Animaux de compagnie: {Animaux}";
+        }
+	}
 }
 
 
