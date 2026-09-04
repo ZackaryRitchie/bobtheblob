514da5308f9d6eb52a009641a89ed552e22a4e98
2551478 <2551478@cegepoutaouais.qc.ca>
2026-09-04 14:17:04 -0400

    ajoute de la classe abstrait animal et completer

---
+++ LaVieDeBob/model/Animal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace LaVieDeBob.model
+{
+    public abstract class Animal
+    {
+        public string Nom { get; set; }
+        public string Type {  get; set; }
+        public bool Faim { get; set; }
+        public int Cout { get; set; }
+        public TimeSpan TempsEntreSouper { get; set; }
+        public DateTime DernierSouper { get; set; }
+    
+    
+        protected Animal() 
+        {
+            Nom=string.Empty;
+            Type = string.Empty;
+            Faim = false;
+            Cout= 0;
+            TempsEntreSouper = TimeSpan.Zero;
+            DernierSouper = DateTime.Now;
+
+
+
+        }
+
+
+        public  string AvertirFaim()
+        {
+            Random random= new Random();
+            int randome=random.Next(1,2);
+
+            if (randome==1 & (DernierSouper-DateTime.Now)< TempsEntreSouper)
+            {
+                return $"{Nom} a faim";
+
+            }
+            return $"{Nom} n'a pas faim";
+        } 
+
+
+        public void Manger(DateTime temps)
+        {
+            Faim = false;
+            DernierSouper=temps;
+        }
+        
+    
+    }
+}
