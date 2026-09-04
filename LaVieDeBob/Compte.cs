e2fa9608740a35ea9ade54a287f4f9ba093ff646
2551478 <2551478@cegepoutaouais.qc.ca>
2026-09-04 13:22:16 -0400

    ajoute classe conpte et completer

---
+++ LaVieDeBob/Compte.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LaVieDeBob
+{
+    public class Compte
+    {
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public int Solde { get; set; }
+    
+        
+        public Compte() 
+        {
+            Prenom = string.Empty;
+            Nom = string.Empty;
+            Solde = 0;
+        }
+
+        public Compte(string prenom, string nom)
+        {
+            Prenom=prenom;
+            Nom = nom;
+            Solde = 0;
+
+        }
+
+
+        public void AjouteArgent(int nbargent)
+        {
+            Solde += nbargent;
+        }
+
+        public void SoustraireAregnet(int nbargent)
+        {
+            Solde -= nbargent;
+        }
+
+        public int Consulter()
+        {  return Solde; }
+        
+ 
+
+
+        public override string ToString()
+        {
+            return $"le compte de {Prenom} {Nom} as {Solde}$";
+        }
+    }
+}
